b68c08f68a860f4980bc7ace637d33118fc0bde3
tirthapucse <tirtha.pucse2018@gmail.com>
2023-10-28 03:51:42 +0600

    ServiceShowcase

---
+++ app/Livewire/ShowHome.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Service;
+use Livewire\Component;
+
+class ShowHome extends Component
+{
+    public function render()
+    {
+
+        $services = Service::orderBy('title','DESC')->get();
+        return view('livewire.show-home',[
+            'services' => $services 
+        ]);
+    }
+}
